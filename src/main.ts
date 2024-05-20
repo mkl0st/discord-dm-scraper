5e0419352206166e4f1ab340f00f90b2244090de
mkl0st <mklost@proton.me>
2024-05-20 12:26:59 +0200

    Logging message attachments

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
+interface Attachment {
+  id: string;
+  filename: string;
+  size: number;
+  url: string;
+  proxy_url: string;
+}
+
 interface Message {
   id: string;
+  type: number;
   content: string;
   timestamp: string;
+  attachments: Attachment[];
+  pinned: boolean;
+  author: {
+    id: string;
+    username: string;
+  };
 }
 
 const getAPIData = async <T>(endpoint: string): Promise<T> => {
@@ -14,11 +29,30 @@ const getAPIData = async <T>(endpoint: string): Promise<T> => {
   return data;
 };
 
+const formatTimestamp = (timestamp: string): string => {
+  const date = new Date(timestamp);
+  const datePart = `${date.getFullYear()}/${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}`;
+  const timePart = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
+  return `${datePart} ${timePart}`;
+};
+
+const getAuthorNickFromAuthorId = (authorId: string): string => {
+  if (authorId === Bun.env.AUTHOR_ONE_ID) return Bun.env.AUTHOR_ONE_NICK || '';
+  return Bun.env.AUTHOR_TWO_NICK || '';
+}
+
 (async () => {
-  const messages = await getAPIData<Message[]>(
+  const messages = (await getAPIData<Message[]>(
     `/channels/${Bun.env.DM_CHANNEL_ID}/messages?after=${Bun.env.FIRST_MESSAGE_ID}&limit=${Bun.env.MESSAGE_LIMIT}`,
-  );
+  )).reverse();
   messages.forEach((message) => {
-    console.log(message.content);
+    const nick = getAuthorNickFromAuthorId(message.author.id);
+    if (message.content) {
+      console.log(`[${formatTimestamp(message.timestamp)}][${nick}] ${message.content}`);
+    }
+    message.attachments?.forEach((attachment) => {
+      console.log(`[${formatTimestamp(message.timestamp)}][${nick}] <${message.id}_${attachment.filename}>`);
+      return;
+    });
   });
 })();
